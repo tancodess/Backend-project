87924beb0be7398772df98418cc74ba63857d308
Tanisha <tanibhadrak8@gmail.com>
2025-03-16 21:15:13 +0530

    register controller bugs fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,24 +13,27 @@ const registerUser = asyncHandler(async (req,res)=>{
     //remove password and refresh token field from response 
     //check for user response
     //return response
-    const {fullname,email,username,password}=req.body
-    console.log(fullname," ",email);
+    const {fullName,email,username,password}=req.body
     // if(fullname===""){
     //     throw new ApiError(400,"Full name is required")
     // }
     if(
-        [fullname,email,username,password].some((field)=>field?.trim()==="" )
+        [fullName,email,username,password].some((field)=>field?.trim()==="" )
     ){
         throw new ApiError(400, "All fields are required!")
     }
-const existedUser = User.findOne({
+const existedUser = await User.findOne({
     $or:[{username},{email}]
 })
 if(existedUser){
     throw new ApiError(409,"User with email or username already exists!")
 }
 const avatarLocalPath = req.files?.avatar[0]?.path;
-const coverImageLocalPath = req.files?.coverImage[0]?.path;
+let coverImageLocalPath;
+if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+    coverImageLocalPath=req.files.coverImage[0].path
+}
+
 if(!avatarLocalPath){
     throw new ApiError(400,"Avatar file is required");
 }
@@ -40,7 +43,7 @@ if(!avatar){
     throw new ApiError(400,"Avatar file is required");
 }
 const user = await User.create({
-    fullname,
+    fullName,
     avatar:avatar.url,
     coverImage:coverImage?.url ||"",
     email,
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,8 @@
-import multer from 'multer'
-const storage=multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,"./public/temp")
-    },
-    filename:function(req,file,cb){
-        cb(null,file.originalname)
-    }
-})
-export const upload = multer({
-    storage,
-})
+import multer from "multer";
+ 
+const storage = multer.diskStorage({ destination: "../../public/filesUpload",
+    filename: (req, file, cb) => { cb(null, Date.now() + "-" + file.originalname); }, });
+   
+ export const upload = multer({ 
+     storage, 
+ })
